b8d5ab27178a550c5863f74ded44cdc6629a0237
jinbkim <topmedia3@naver.com>
2021-08-13 17:07:34 +0900

    ADD : override annotaion example

---
+++ overrideAnnotation/README.md
@@ -0,0 +1,7 @@
+# @Override annotaion 예제
+
+## 기술스택
+- Java
+
+## 참고
+- https://maktooob.tistory.com/79
---
+++ overrideAnnotation/overrideAnnotation.java
@@ -0,0 +1,37 @@
+class Man {
+	String name;
+
+	public Man(String name) {
+		this.name = name;
+	}
+
+	public void introduce() {
+		System.out.println("name : " + name);
+	}
+}
+
+class Student extends Man {
+	int score;
+
+	public Student(String name, int score) {
+		super(name);  // 부모 클래스의 생성자 호출
+		this.score = score;
+	}
+
+	@Override  // 부모클래스에 introduce 메소드가 없다면 에러
+	public void introduce() {
+		super.introduce();  // 부모 클래스의 introduce 메소드 호출
+		System.out.println("score : " + score);
+	}
+}
+
+public class overrideAnnotation {
+	public static void main(String[] args) {
+		Man m = new Man("jinbkim");
+		Student s = new Student("hjung", 100);
+
+		m.introduce();
+		System.out.println();
+		s.introduce();
+	}
+}
